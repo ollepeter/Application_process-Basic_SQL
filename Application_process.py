f0d2069df35cf34c8400f2b61a822a9332fc3491
ollepeter <mr.peterolle@gmail.com>
2017-05-31 13:35:16 +0200

    routing and initial funcions addad for the 6 features

--- Application_process.py
+++ Application_process.py
@@ -1,12 +1,58 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def show_menu():
-    '''Renders the SQL Query Menu'''
-    pass
+    '''Renders the Query Menu'''
+    return render_template('index.html')
+
+
+@app.route("/mentors")
+def mentors():
+    '''Renders the query result with:
+        - name of the mentors
+        - name and country of schools'''
+    return 'Mentors'
+
+
+@app.route("/all-school")
+def all_school():
+    '''Renders the query result with:
+        - name of the mentors
+        - name and country of schools extended'''
+    return 'All School'
+
+
+@app.route("/mentors-by-country")
+def mentors_by_country():
+    '''Renders the query result with:
+        - country
+        - number of mentors'''
+    return 'Mentors by Country'
+
+
+@app.route("/contacts")
+def contacts():
+    '''Renders the query result with:
+        - country
+        - number of mentors'''
+    return 'Contacts'
+
+
+@app.route("/applicants")
+def applicants():
+    '''Renders the query result with:
+        - name of appliciants
+        - name of assigned mentors'''
+    return 'Applicants'
+
+
+@app.route("/applicants-and-mentors")
+def appliciants_and_mentors():
+    return 'Appliciants and Mentors'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
